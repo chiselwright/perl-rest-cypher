432ad65d61dda2ab22414965d37cb0b217bb275d
Chisel <chisel@chizography.net>
2015-10-22 10:08:29 +0100

    Add missing POD for as_string in ::Exception

--- lib/REST/Cypher/Exception.pm
+++ lib/REST/Cypher/Exception.pm
@@ -9,6 +9,16 @@ package REST::Cypher::Exception {
         q{""}    => 'as_string',
         fallback => 1;
 
+=pod
+
+=head2 as_string
+
+This method in L<REST::Cypher::Exception> allows easy stringification of
+thrown exceptions.
+
+It returns the current value of I<message>.
+
+=cut
     sub as_string {
         my ($self) = @_;
         return $self->message;
